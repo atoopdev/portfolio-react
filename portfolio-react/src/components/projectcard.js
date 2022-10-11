81617dc3e18933bc3489a98d16d777053f921f36
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-11 10:28:14 -0700

    more css

--- portfolio-react/src/components/projectcard.js
+++ portfolio-react/src/components/projectcard.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function ProjectCard(props){
     console.log("isMainPage: ", props.isMainPage)
-    
+
     return <div className="project-Card">
         <h1 className="project-title">{props.project.title}</h1>
         <a href={props.project.link} className="project-btn"><img src={props.project.img} alt={props.project.title}></img></a>
