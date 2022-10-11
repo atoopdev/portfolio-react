99f0c568a9e3b02878b2664cbda7dd046b835ea9
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-10-11 12:47:03 -0700

    basic About section css styling
    had to remove srcset and picture tags. revisit later.

--- portfolio-react/src/components/about.js
+++ portfolio-react/src/components/about.js
@@ -2,20 +2,20 @@ import React from "react"
 import AmandaImg from "../images/portfolio_header.JPG"
 
 export default function About(){
-    return <div className="About">
+    return <header className="About">
         <div className="content_wrapper">
         <h1>Amanda Toop</h1>
         <h2>Information Technology Specialist</h2>
-        <picture>
-        <source media="(max-width:374px)" srcset={AmandaImg}>
-              <img src={AmandaImg} alt="Picture of Amanda">
-        </picture>
-        <p>I love creating products that use technology to solve problems. To me, every problem is a fresh adventure and I really enjoy that. For the majority of my working career this has resulted in roles that emphasize providing technology solutions, business analysis, and working closely with users. I find bringing bringing order to chaos exciting!</p>
+        <div className="header-item">
+            <img src={AmandaImg} alt="Picture of Amanda"></img>
+        
+            <p>I love creating products that use technology to solve problems. To me, every problem is a fresh adventure and I really enjoy that. For the majority of my working career this has resulted in roles that emphasize providing technology solutions, business analysis, and working closely with users. I find bringing bringing order to chaos exciting!</p>
             
-          <P>My current focus is on web development. I really enjoy creating products that are easy to use, efficient, and (ideally) look aesthetically pleasing. </P>
+            <p>My current focus is on web development. I really enjoy creating products that are easy to use, efficient, and (ideally) look aesthetically pleasing. </p>
 
-          <p>Outside of technological interests I am an avid cook and Peloton devotee. On the weekends you would find me at a local museum or gallery, or trying a new restaurant in the International District.</p>
+            <p>Outside of technological interests I am an avid cook and Peloton devotee. On the weekends you would find me at a local museum or gallery, or trying a new restaurant in the International District.</p>
+          </div>
         </div>
        
-    </div>
+    </header>
 }
